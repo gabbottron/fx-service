62cecb40906d4c0e069ed31982f5994b4d3f2ca4
Geoffrey Abbott <geoffrey.abbott@arcules.com>
2024-05-28 17:07:02 -0400

    added a muxer and the server now handles a route

--- main.go
+++ main.go
@@ -3,15 +3,17 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"net"
 	"net/http"
+	"os"
 
 	"go.uber.org/fx"
 )
 
 // Function to build the HTTP server
-func NewHTTPServer(lc fx.Lifecycle) *http.Server {
-	srv := &http.Server{Addr: ":8080"}
+func NewHTTPServer(lc fx.Lifecycle, mux *http.ServeMux) *http.Server {
+	srv := &http.Server{Addr: ":8080", Handler: mux}
 	lc.Append(fx.Hook{
 		OnStart: func(ctx context.Context) error {
 			ln, err := net.Listen("tcp", srv.Addr)
@@ -29,11 +31,23 @@ func NewHTTPServer(lc fx.Lifecycle) *http.Server {
 	return srv
 }
 
+// NewServeMux builds a ServeMux that will route requests
+// to the given EchoHandler.
+func NewServeMux(echo *EchoHandler) *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.Handle("/echo", echo)
+	return mux
+}
+
 func main() {
 	fx.New(
 		// This provides the HTTP Server to the container
 		// so that we may use it
-		fx.Provide(NewHTTPServer),
+		fx.Provide(
+			NewHTTPServer,
+			NewServeMux,
+			NewEchoHandler,
+		),
 		// Used for root level invocations like background
 		// workers or global loggers. Without this invoke,
 		// the lifecycle methods for the HTTP server we set
@@ -42,4 +56,20 @@ func main() {
 		// is not directly referenced in code yet.
 		fx.Invoke(func(*http.Server) {}),
 	).Run()
+}
+
+// EchoHandler is an http.Handler that copies its request body
+// back to the response.
+type EchoHandler struct{}
+
+// NewEchoHandler builds a new EchoHandler.
+func NewEchoHandler() *EchoHandler {
+	return &EchoHandler{}
+}
+
+// ServeHTTP handles an HTTP request to the /echo endpoint.
+func (*EchoHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if _, err := io.Copy(w, r.Body); err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to handle request:", err)
+	}
 }
